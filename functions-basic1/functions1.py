977fbca83fd5bfa367262c1fc1791ae505bcdb78
Mgarcia456 <Asvp_miguel36@icloud.com>
2023-02-06 15:58:08 -0500

    PythonStack

---
+++ functions-basic1/functions1.py
@@ -0,0 +1,134 @@
+#    5
+
+def number_of_food_groups():
+    return 5
+print(number_of_food_groups())
+
+#         
+#2 had to add a def to other variable for function or otherwise error
+def number_of_military_branches():
+    return 5
+def number_of_days_in_a_week_silicon_or_triangle_sides():
+    return 7
+print(number_of_days_in_a_week_silicon_or_triangle_sides() + number_of_military_branches())
+#                 5
+
+def number_of_books_on_hold():
+    return 5
+    return 10
+print(number_of_books_on_hold())
+#            print (10) doesnt belong to anythiung so it doesnt print so it will return 5
+def number_of_fingers():
+    return 5
+    print(10)
+print(number_of_fingers())
+
+#           5      and none because we need to x + 5 to print(x) to output 5 instead of none
+
+def number_of_great_lakes():
+    print(5)
+x = number_of_great_lakes()
+print(x)
+
+#6                 8 
+def add(b,c):
+    print(b+c)
+print(add(1,2) + add(2,3))
+
+#7           error
+def concatenate(b,c):
+    return str(b)+str(c)
+    print(concatenate(2,5))
+
+#8         10
+def number_of_oceans_or_fingers_or_continents():
+    b = 100
+    print(b)
+    if b < 10:
+        return 5
+    else:
+      return 10
+      return 7
+print(number_of_oceans_or_fingers_or_continents())
+
+#9   1st 7    2nd 14     3rd 21
+def number_of_days_in_a_week_silicon_or_triangle_sides(b,c):
+    if b<c:
+        return 7
+    else:
+        return 14
+    return 3
+print(number_of_days_in_a_week_silicon_or_triangle_sides(2,3))
+print(number_of_days_in_a_week_silicon_or_triangle_sides(5,3))
+print(number_of_days_in_a_week_silicon_or_triangle_sides(2,3) + number_of_days_in_a_week_silicon_or_triangle_sides(5,3))
+
+#10           8
+def addition(b,c):
+    return b+c
+    return 10
+print(addition(3,5))
+
+#11            500
+b = 500
+print(b)
+def foobar():
+    b = 300
+    print(b)
+print(b)
+foobar()
+print(b)
+
+#12              300
+b = 500
+print(b)
+def foobar():
+    b = 300
+    print(b)
+    return b
+print(b)
+foobar()
+print(b)
+
+#13            300
+b = 500
+print(b)
+def foobar():
+    b = 300
+    print(b)
+    return b
+print(b)
+b=foobar()
+print(b)
+
+#14       1 2 3
+def foo():
+    print(1)
+    bar()
+    print(2)
+def bar():
+    print(3)
+foo()
+
+#15           10 and 5
+def foo():
+    print(1)
+    x = bar()
+    print(x)
+    return 10
+def bar():
+    print(3)
+    return 5
+y = foo()
+print(y)
+
+
+
+
+
+
+
+
+
+
+
+
